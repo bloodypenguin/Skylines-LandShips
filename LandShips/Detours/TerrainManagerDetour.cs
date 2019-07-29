0bd8fe6a177625baa826249c9b1603a54e3d7138
Bloody Penguin <bloodypenguin.skylines@gmail.com>
2019-07-29 22:49:26 +0200

    Add to source control

---
+++ LandShips/Detours/TerrainManagerDetour.cs
@@ -0,0 +1,17 @@
+﻿using LandShips.Redirection;
+using UnityEngine;
+
+namespace LandShips.Detours
+{
+    [TargetType(typeof(TerrainManager))]
+    public class TerrainManagerDetour : TerrainManager
+    {
+        [RedirectMethod]
+        public new float SampleBlockHeightSmoothWithWater(Vector3 worldPos, bool timeLerp, float waterOffset, out bool hasWater)
+        {
+            var waterLevel = this.SampleBlockHeightSmoothWithWater((float)((double)worldPos.x / 16.0 + 540.0), (float)((double)worldPos.z / 16.0 + 540.0), timeLerp, waterOffset, out hasWater) * (1f / 64f);
+            hasWater = true;
+            return waterLevel;
+        }
+    }
+}
---
+++ LandShips/Mod.cs
@@ -0,0 +1,11 @@
+﻿using ICities;
+
+namespace LandShips
+{
+    public class Mod : IUserMod
+    {
+        public string Name => "Land Ships";
+
+        public string Description => "Restores good old ship behavior that allows them to float on the load";
+    }
+}
